cfd653cae0b71f0bf52e7b5975a9ffa9bf8bc47a
chiragSahani <chiragsahani093@gmail.com>
2025-04-01 13:55:32 +0000

    Solution

---
+++ src/components/ActiveEventRegistrationDetails/index.js
@@ -0,0 +1,88 @@
+// Write your code here
+import './index.css'
+
+const registrationStatusConstants = {
+  yetToRegister: 'YET_TO_REGISTER',
+  registered: 'REGISTERED',
+  registrationsClosed: 'REGISTRATIONS_CLOSED',
+}
+
+const ActiveEventRegistrationDetails = props => {
+  const {activeEventRegistrationStatus} = props
+
+  const renderNoActiveEventView = () => (
+    <div className="no-active-event-container">
+      <p className="no-active-event-description">
+        Click on an event, to view its registration details
+      </p>
+    </div>
+  )
+
+  const renderYetToRegisterView = () => (
+    <div className="view-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/events-register-img.png"
+        alt="yet to register"
+        className="registration-image"
+      />
+      <p className="registration-description">
+        A live performance brings so much to your relationship with dance.
+        Seeing dance live can often make you fall totally in love with this
+        beautiful art form.
+      </p>
+      <button type="button" className="register-button">
+        Register Here
+      </button>
+    </div>
+  )
+
+  const renderRegisteredView = () => (
+    <div className="view-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/events-regestered-img.png"
+        alt="registered"
+        className="registration-status-image"
+      />
+      <h1 className="registered-heading">
+        You have already registered for the event
+      </h1>
+    </div>
+  )
+
+  const renderRegistrationsClosedView = () => (
+    <div className="view-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/events-registrations-closed-img.png"
+        alt="registrations closed"
+        className="registration-image"
+      />
+      <h1 className="registrations-closed-heading">
+        Registrations Are Closed Now!
+      </h1>
+      <p className="registrations-closed-description">
+        Stay tuned. We will reopen the registrations soon!
+      </p>
+    </div>
+  )
+
+  const renderActiveEventRegistrationDetails = () => {
+    switch (activeEventRegistrationStatus) {
+      case registrationStatusConstants.yetToRegister:
+        return renderYetToRegisterView()
+      case registrationStatusConstants.registered:
+        return renderRegisteredView()
+      case registrationStatusConstants.registrationsClosed:
+        return renderRegistrationsClosedView()
+      default:
+        return renderNoActiveEventView()
+    }
+  }
+
+  return (
+    <div className="registration-status-container">
+      {renderActiveEventRegistrationDetails()}
+    </div>
+  )
+}
+
+export default ActiveEventRegistrationDetails
---
+++ src/components/EventItem/index.js
@@ -0,0 +1,23 @@
+// Write your code here
+import './index.css'
+
+const EventItem = props => {
+  const {eventDetails, setActiveEventId} = props
+  const {id, imageUrl, name, location} = eventDetails
+
+  const onClickEvent = () => {
+    setActiveEventId(id)
+  }
+
+  return (
+    <li className="event-item">
+      <button type="button" className="event-button" onClick={onClickEvent}>
+        <img src={imageUrl} alt="event" className="event-image" />
+      </button>
+      <p className="name">{name}</p>
+      <p className="location">{location}</p>
+    </li>
+  )
+}
+
+export default EventItem
